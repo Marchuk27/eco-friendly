ef5912e28dc790acc1bdb4e55927bac9352483aa
Anton Sakharov <ya.antonsakharov@yandex.ru>
2020-03-04 22:31:29 +0500

    Company model

--- src/main/java/org/ecofriendly/model/Company.java
+++ src/main/java/org/ecofriendly/model/Company.java
@@ -1,7 +1,81 @@
 package org.ecofriendly.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Company {
+	@Id
+	private int    id;
+	private String address;
+	private String category;
+	private String email;
+	private String entity;
+	private double latitude;
+	private double longitude;
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEntity() {
+		return entity;
+	}
+
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
