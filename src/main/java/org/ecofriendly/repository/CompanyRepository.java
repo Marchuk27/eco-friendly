2aa9958eefb331dd64208cfc4c570dae601242d5
Anton Sakharov <ya.antonsakharov@yandex.ru>
2020-03-04 23:14:15 +0500

    PK type fix

--- src/main/java/org/ecofriendly/repository/CompanyRepository.java
+++ src/main/java/org/ecofriendly/repository/CompanyRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends CrudRepository<Company, String> {
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
 	Company findByName(String entity);
 }
