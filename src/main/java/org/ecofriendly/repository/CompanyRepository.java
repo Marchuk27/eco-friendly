9ee851b79e046d9d93ee1c1f1eaee77f6e5a2faf
Anton Sakharov <ya.antonsakharov@yandex.ru>
2020-03-04 22:35:52 +0500

    Company repository

--- src/main/java/org/ecofriendly/repository/CompanyRepository.java
+++ src/main/java/org/ecofriendly/repository/CompanyRepository.java
@@ -1,4 +1,10 @@
 package org.ecofriendly.repository;
 
-public interface CompanyRepository {
+import org.ecofriendly.model.Company;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, String> {
+	Company findByEntity(String entity);
 }
