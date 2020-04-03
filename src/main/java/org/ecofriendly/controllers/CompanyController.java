0c79d88c13e0b02aafc631b87c5a76b08b35ead0
Anton Sakharov <ya.antonsakharov@yandex.ru>
2020-04-03 21:56:56 +0500

    findAllByName

--- src/main/java/org/ecofriendly/controllers/CompanyController.java
+++ src/main/java/org/ecofriendly/controllers/CompanyController.java
@@ -3,6 +3,7 @@ package org.ecofriendly.controllers;
 import org.ecofriendly.db.entity.Company;
 import org.ecofriendly.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,9 @@ public class CompanyController {
 	@RequestMapping(path = "/")
 	public Optional<Company> getCompanyById(@RequestParam int id){
 		return companyService.getCompanyById(id);
+	}
+	@RequestMapping("/{name}")
+	public Iterable<Company> findAllCompaniesByName(@PathVariable String name){
+		return companyService.findAllCompaniesByName(name);
 	}
 }
