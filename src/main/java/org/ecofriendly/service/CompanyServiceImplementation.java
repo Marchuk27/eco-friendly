e7bf93a156bca8d88df3336013312a7ec45d459c
Anton Sakharov <ya.antonsakharov@yandex.ru>
2020-04-19 20:24:50 +0500

    Service: GetCategoryCompanies

--- src/main/java/org/ecofriendly/service/CompanyServiceImplementation.java
+++ src/main/java/org/ecofriendly/service/CompanyServiceImplementation.java
@@ -6,6 +6,8 @@ import org.ecofriendly.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +35,15 @@ public class CompanyServiceImplementation implements CompanyService {
 	@Override
 	public Iterable<Company> findAllCompaniesByCategories(Iterable<Category> categories) {
 		return companyRepository.findAllByCategoriesIn(categories);
+	}
+
+	@Override
+	public Dictionary<String, List<Company>> getCategoryCompanies(Iterable<Category> categories) {
+		Dictionary<String, List<Company>> dictionary = new Hashtable<>();
+		for (Category category : categories) {
+			dictionary.put(category.getName(), (List<Company>) companyRepository.findAllByCategoriesIs(category));
+		}
+		return dictionary;
 	}
 
 }
