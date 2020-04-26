728757e8f572f86ac12651599bccbe11cfd177a8
Marchuk27 <aleksandr.marhcuk.98@mail.ru>
2020-04-26 22:42:51 +0500

    remove method

--- src/main/java/org/ecofriendly/repository/UserAccountRepository.java
+++ src/main/java/org/ecofriendly/repository/UserAccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
-	UserAccount findByEmail(String email);
+
 
 	UserAccount findUserAccountByEmail(String email);
 }
