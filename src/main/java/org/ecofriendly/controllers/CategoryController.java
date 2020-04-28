f7c4de4df499011a1c1f9e87c4fcc44152dbd0d7
Marchuk27 <aleksandr.marhcuk.98@mail.ru>
2020-04-28 19:23:42 +0500

    check for null

--- src/main/java/org/ecofriendly/controllers/CategoryController.java
+++ src/main/java/org/ecofriendly/controllers/CategoryController.java
@@ -1,16 +1,20 @@
 package org.ecofriendly.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ecofriendly.db.entity.Company;
 import org.ecofriendly.db.entity.company.Category;
 import org.ecofriendly.service.interfaces.ICompanyService;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Dictionary;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
+@Slf4j
 public class CategoryController {
 	private final ICompanyService companyService;
 
@@ -20,6 +24,10 @@ public class CategoryController {
 
 	@GetMapping("/category/")
 	public Dictionary<String, List<Company>> getCategoryCompaniesDictionary(@RequestParam List<Category> category) {
+		Dictionary<String, List<Company>> companyDictionary =  companyService.getCategoryCompanies(category);
+		if (Objects.isNull(companyDictionary) || ObjectUtils.isEmpty(companyDictionary)) {
+			log.error("В БД отсутствуют компании");
+		}
 		return companyService.getCategoryCompanies(category);
 	}
 }
