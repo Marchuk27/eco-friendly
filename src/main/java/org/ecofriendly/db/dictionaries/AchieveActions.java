1442b7070c5dc17f03b4af0f098efaf9a97c7e1d
Marchuk27 <aleksandr.marhcuk.98@mail.ru>
2020-05-07 16:50:43 +0500

    Сущность для достижений

--- src/main/java/org/ecofriendly/db/dictionaries/AchieveActions.java
+++ src/main/java/org/ecofriendly/db/dictionaries/AchieveActions.java
@@ -8,66 +8,65 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * Справочник, в котором будут храниться достижения
+ * Аналитика по достижениям будет указана внизу класса, а также
+ * в Trello будет прикреплен соответствующий документ
+ * Также в javadoc будет указано место инкрементации/проверки достижения в коде (Класс, Метод)
+ */
 @Getter
 @Setter
-//@Entity
+@Entity
 public class AchieveActions {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    /** "Безумец" - сдал что-то из категории "Опасное" */
-    private Boolean hasDangerInTracker;
+    private String name;
+    private String description;
+    private String urlToIcon;
+
 
     /**
+     *  Формат: Нэйминг достижения / Текстовка /  Место в коде
+     *
+     * "Эко-энтузиаст" / Зарегистрируйтесь в личном кабинете
+     * / UserAccountController.class; method = registerUser();
+     *
+     * //TODO: Доделать оставшующся аналитику
+     * "Безумец" / Сдайте что-либо из категории "Опасное" /
+     *
      * "Начинающий сортировщик" >= 500 в общем единиц в трекере
      * "Продвинутый сортировщик" >= 1000
      * "Ярый сортировщик" >= 5000
      * "Я из Швеции" >= 10000
-     * */
-    private Integer generalUnitAmountInTracker;
-
-    /**
+     *
      * "Начинающий стекольщик" >= 30
      * "Продвинутый стекольщик" >= 500
      * "Ярый стекольщик" >= 2000
-     * */
-    private Integer glassAmountInTracker;
-
-    /**
+     *
      * «Рекрут-краснолюд» - сдать более 10 единиц металла
      * «Краснолюд» - сдать более 500 единиц металла
      * «Мастер краснолюдов» - сдать более 2000 единиц металла
-     */
-    private Integer metalAmountInTracker;
-
-    /** «Все и сразу» - отметить в трекере все категории за один раз */
-    private Boolean allInputsAreFilledInTracker;
-
-    /** "Идейный" - закрепить 2 идеи в чеклисте*/
-    private Boolean hasTwoIdeasInCheckList;
-
-    /**
+     *
+     * «Все и сразу» - отметить в трекере все категории за один раз
+     *
+     * "Идейный" - закрепить 2 идеи в чеклисте
+     *
+     *
      * "Турист" - просмотреть на карте 10 компаний*
      * "Матёрый географ" - 70 компаний
      * "Географ глобус сдал" - 250 компаний
      * "Экскурсовод" - 700 компаний
-     */
-    private Integer amountOfCompaniesViewed;
-
-    /**
+     *
      * "Пока учусь" - зайти 5 раз на страницу FAQ
      * "Студент" - 30 раз
      * "Исследователь" - 100 раз
      * "Вместо Гугл" - 500 раз
-     * */
-    private Integer amountOfVisitedFAQ;
-
-    /**
+     *
      * "Интересуюсь" - зайти 5 раз на страницу Новости
      * "Событийный" - 30 раз на страницу Новости
      * "Газета подождёт" - 70 раз
      * "На лавке у подьезда" - 150 раз
-     * */
-    private Integer amountOfVisitedNews;
+     */
 }
