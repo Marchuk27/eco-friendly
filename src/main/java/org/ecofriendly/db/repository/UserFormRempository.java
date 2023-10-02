60c2c85cce468526f735b8248a806e4bec6cfeb5
Marchuk27 <aleksandr.marchuk.98@mail.ru>
2023-10-02 18:19:56 +0500

    ecofriendly: test iRebase

--- src/main/java/org/ecofriendly/db/repository/UserFormRempository.java
+++ src/main/java/org/ecofriendly/db/repository/UserFormRempository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyFormRepository extends JpaRepository<CompanyForm, Long> {
+public interface UserFormRempository extends JpaRepository<CompanyForm, Long> {
 }
